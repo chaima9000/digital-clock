17cf79a803ed83fae65326b3328df88a4f1ab508
chaima <chaimakhouya85@gmail.com>
2026-08-19 14:40:53 +0100

    end program

---
+++ index.js
@@ -0,0 +1,11 @@
+function update() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2,0);
+    const minute = now.getMinutes().toString().padStart(2,0);
+    const second = now.getSeconds().toString().padStart(2,0);
+
+    const timestring = `${hours}:${minute}:${second}`;
+    document.getElementById("clock").textContent = timestring;
+}
+update();
+setInterval(update, 1000);
