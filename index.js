a06528d973f9418cf245143c73e080592ea9854b
chaima <chaimakhouya85@gmail.com>
2026-08-19 14:48:31 +0100

    end program

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function update() {
     const hours = now.getHours().toString().padStart(2,0);
     const minute = now.getMinutes().toString().padStart(2,0);
     const second = now.getSeconds().toString().padStart(2,0);
+    
 
     const timestring = `${hours}:${minute}:${second}`;
     document.getElementById("clock").textContent = timestring;
